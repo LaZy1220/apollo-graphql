613f93f4b3789676421af74903f6baa62ed24ea0
LaZy1220 <4elove4iska@gmail.com>
2022-08-06 11:16:12 +0300

    styled right side productpage

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -9,14 +9,33 @@ const Info = styled.div``
 const ProductImgs = styled.div``
 const MainImg = styled.div``
 const AltImgs = styled.div``
-const ProductTitle = styled.div``
-const ProductBrend = styled.div``
+const ProductTitle = styled.div`
+    font-size: 30px;
+    font-family: var(--raleway);
+    font-weight: var(--fw-hard2);
+`
+const ProductBrend = styled.div`
+    font-size: 30px;
+    margin-top: 43px;
+`
 const Parameters = styled.div``
 const BoxItems = styled.div`
     display:flex;
     gap:12px;
 `
-const AddCartButton = styled.button``
+const AddCartButton = styled.div`
+    margin:20px 0 40px;
+    width: 292px;
+    height: 52px;
+    background-color: #5ECE7B;
+    color:white;
+    font-size: 16px;
+    display: flex;
+    align-items:center;
+    justify-content: center;
+    font-weight: var(--fw-hard);
+    cursor: pointer;
+`
 const Price = styled.div``
 const Box = styled.div`
     display:flex;
@@ -24,7 +43,7 @@ const Box = styled.div`
     justify-content:center;
     height: 45px;
     width: 63px;
-    border:1px solid #1D1F22;
+    border:1px solid var(--black);
     cursor: pointer;
 `
 
@@ -58,26 +77,31 @@ export const ProductPage = ({
                         <ProductBrend>{infoProduct?.brand}</ProductBrend>
                         {infoProduct?.attributes.map(atr=>
                             (<Parameters key={atr.id}>
-                                <span>{atr?.name}:</span>
+                                <span style={{ padding:'24px 0 8px',fontSize:'18px',fontWeight:'var(--fw-bold)',display:'block'}}>{(atr?.name).toUpperCase()}:</span>
                                 <BoxItems>
                                 {
                                     atr.items.map(item=>(
-                                        <Box
-                                            key={item.id}>
-                                                <span style={{letterSpacing:'0.05em',fontFamily:'var(--sspro)'}}>{item.displayValue}</span>
-                                        </Box> 
+                                        atr.id==='Color'
+                                        ?<Box style={{backgroundColor:`${item.value}`,height:'32px',width:'32px'}} key={item.id}/>
+                                        :(<Box 
+                                        key={item.id}>
+                                            <span style={{letterSpacing:'0.05em',fontFamily:'var(--sspro)'}}>
+                                                    <span>{item.value}</span>
+                                            </span>
+                                    </Box>) 
                                     ))
                                 }
                                 </BoxItems>
-                            </Parameters>))}
+                             </Parameters>))}
+                             <p style={{fontSize:'18px',fontWeight:'var(--fw-bold)',margin:'38px 0 10px'}}>PRICE:</p>
                             {
                         infoProduct?.prices.map(price=>(
                             price.currency.symbol===currentCurrency.symbol
-                            &&<Price key={price.amount}><span style={{fontFamily:'var(--raleway)',fontWeight:'var(--fw-hard)'}}>{currentCurrency.symbol}{price.amount}</span></Price>
+                            &&<Price key={price.amount}><p style={{fontFamily:'var(--family)',fontWeight:'var(--fw-bold)',fontSize:'24px'}}>{currentCurrency.symbol}{price.amount}</p></Price>
                           ))
                         }
                         <AddCartButton>ADD TO CART</AddCartButton>
-                        <p>{infoProduct?.description}</p>
+                        <div dangerouslySetInnerHTML={{__html:infoProduct?.description}}></div>
                     </Info>
                 </Details>
             }
