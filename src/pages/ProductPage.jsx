ae419de4226775123abf2f0fe9590fdfa107f805
LaZy1220 <4elove4iska@gmail.com>
2022-08-17 18:50:13 +0300

    add style to change atributes

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -135,20 +135,32 @@ export const ProductPage = ({
                                 <span style={{padding:'24px 0 8px',fontSize:'18px',fontFamily:'var(--roboto)',fontWeight:'var(--fw-bold)',display:'block'}}>{(atr?.name).toUpperCase()}:</span>
                                 <BoxItems>
                                 {
-                                    atr.items.map((item,index)=>(
+                                    atr.items.map((item)=>(
                                         atr.id==='Color'
                                         ?<ColorBox
-                                            className={index===0?'activeColor':''}
+                                            className={chooseAttributes.length
+                                                ?chooseAttributes.map(color=>{
+                                                    if(color.value===item.value){
+                                                        return 'activeColor'
+                                                    }
+                                                })
+                                                :''}
                                             id={item.id} 
                                             key={item.id}
                                             onClick={()=>handleChooseAttribute(atr,item)}
-                                            style={{backgroundColor:`${item.value}`,border:`${item.id==='White'?'1px solid var(--black)':'none'}`,height:'32px',width:'32px'}} 
+                                            style={{backgroundColor:`${item.value}`,border:`${item.value ==='#FFFFFF'?'1px solid var(--black)':'none'}`,height:'32px',width:'32px'}}
                                             />                                           
                                         :(<BoxAtribute
                                             key={item.id}
                                             id={item.id}
                                             onClick={()=>handleChooseAttribute(atr,item)}
-                                            className={index===0?'activeSomeParametrs':''}>
+                                            className={chooseAttributes.length
+                                                ?chooseAttributes.map(color=>{
+                                                    if(color.value===item.value){
+                                                        return 'activeSomeParametrs'
+                                                    }
+                                                })
+                                                :''}>
                                             <span style={{letterSpacing:'0.05em',fontFamily:'var(--sspro)'}}>
                                                   <span>{item.value}</span>
                                             </span>
