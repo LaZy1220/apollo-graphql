92108a482cc4c2a901ac1b00d779fc74dcbf4dde
LaZy1220 <4elove4iska@gmail.com>
2022-09-21 21:26:39 +0300

    addCartPage

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,7 +1,41 @@
-export const CartPage = ()=>{
+import styled from "styled-components"
+import { CartItem } from "../components/CartItem"
+
+const CartEl = styled.div`
+padding: 80px 0;
+`
+const CartTitle = styled.h1`
+    font-size:32px;
+    font-weight:var(--fw-bold);
+    text-transform: uppercase;
+    padding-bottom: 55px;
+    border-bottom: 1px solid #E5E5E5;
+`
+export const CartPage = ({
+    order,
+    currentCurrency,
+    incrementQuantity,
+    decrementQuantity,
+    totalPrice,
+    orderCounter,
+})=>{
     return(
-        <div>
-            CART!!!
-        </div>
+        <CartEl>
+            <CartTitle>Cart</CartTitle>
+            {
+                order.map(item=>(
+                    <CartItem 
+                        key={item.id}
+                        item={item}
+                        currentCurrency={currentCurrency}
+                        incrementQuantity={incrementQuantity}
+                        decrementQuantity={decrementQuantity}/>
+                ))
+            }
+            <p></p>
+            <p></p>
+            <p></p>
+            <button></button>
+        </CartEl>
     )
 }
